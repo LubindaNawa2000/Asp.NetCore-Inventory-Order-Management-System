76148ee534a4991ede262dc24fd91f6b729d1b9e
Sentox <sentoxdeadshotnawa@gmail.com>
2024-06-09 11:29:45 +0200

    dotnet new mvc --auth Individual
    Added Initial Modules as template to start development.

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.NetCore_Inventory_Order_Management_System.Data;
+
+public class ApplicationDbContext : IdentityDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+}
