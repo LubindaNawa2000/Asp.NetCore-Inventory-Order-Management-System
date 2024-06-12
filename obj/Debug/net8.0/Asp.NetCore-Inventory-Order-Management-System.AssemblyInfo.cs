56bc4f47ca81435357311f51bb02b8bf665143fa
Sentox <sentoxdeadshotnawa@gmail.com>
2024-06-12 02:15:25 +0200

    binary file change

--- obj/Debug/net8.0/Asp.NetCore-Inventory-Order-Management-System.AssemblyInfo.cs
+++ obj/Debug/net8.0/Asp.NetCore-Inventory-Order-Management-System.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Asp.NetCore-Inventory-Order-Management-System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a16dff05655e31446a3a8f5af0cc64e72806d551")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96197c6198b6eb29aefe24ca02f76b9d11cd4e39")]
 [assembly: System.Reflection.AssemblyProductAttribute("Asp.NetCore-Inventory-Order-Management-System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Asp.NetCore-Inventory-Order-Management-System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
